eee640499203c44e7b254d8620b672985b1c1a75
Guisandroni <gui.sandroni@gmail.com>
2025-02-13 16:40:19 -0300

    Config tailwind and reset project

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      
+
+      className="flex-1 justify-center items-center bg-red-500"
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/screens/welcome">teste</Link>
+    </View>
+  );
+}
